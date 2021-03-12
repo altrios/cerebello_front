f7538ef072173edf71254d369f98603d2366a461
altrios <famartinez.ti@gmail.com>
2021-03-11 23:13:07 -0500

    0.0.0.1: material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import Button
+
 
 function App() {
   return (
     <div >
-      
+
         
       
     </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
+function App() {
+    return (
+            <div>
+            asdasd
+            </div>
+            );
+}
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
+export default App;
