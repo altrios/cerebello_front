6f8557c462d4416d838405e08595c7280d6d3ab8
altrios <famartinez.ti@gmail.com>
2021-06-15 22:05:07 -0500

    arreglar cosas...

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -6,46 +6,59 @@ export default function Logout() {
     const [state, setState] = useContext(AppContext)
     var axios = require('axios');
     let history = useHistory();
-    
-        var data = JSON.stringify({});
-console.log("aja")
-        var config = {
-            method: 'post',
-            url: 'http://cerebelloback.echilateral.com/api/logout',
-            headers: {
-                'Authorization': 'Bearer ' + state.token,
-                'Content-Type': 'application/json'
+
+    var data = JSON.stringify({});
+    console.log("aja")
+    var config = {
+        method: 'post',
+        url: 'http://cerebelloback.echilateral.com/api/logout',
+        headers: {
+            'Authorization': 'Bearer ' + state.token,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
             },
-            data: data
-        };
+        data: data
+    };
 
-        axios(config)
-            .then(function (response) {
-                console.log(response)
-                sessionStorage.removeItem('token');
-                sessionStorage.removeItem('name');
-                sessionStorage.removeItem('email');
-                sessionStorage.removeItem('nivel');
-                sessionStorage.removeItem('title')
-                sessionStorage.removeItem('description')
-                setState('')
-               
-                console.log(state.token)
-               
-                history.push("/")
+    axios(config)
+        .then(function (response) {
+            console.log(response)
+            sessionStorage.removeItem('token');
+            sessionStorage.removeItem('name');
+            sessionStorage.removeItem('email');
+            sessionStorage.removeItem('nivel');
+            sessionStorage.removeItem('title')
+            sessionStorage.removeItem('description')
+            setState('')
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+            console.log(state.token)
+
+            history.push("/")
+
+        })
+        .catch(function (error) {
+            console.log(error);
             
+            sessionStorage.removeItem('token');
+            sessionStorage.removeItem('name');
+            sessionStorage.removeItem('email');
+            sessionStorage.removeItem('nivel');
+            sessionStorage.removeItem('title')
+            sessionStorage.removeItem('description')
+            setState('')
 
-    
+            console.log(state.token)
+
+            history.push("/")
+        });
+
+
+
 
     return (
         <div>
 
-            
+
         </div>
 
     )
