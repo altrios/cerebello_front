b2909ad9ff571fb9209f8a6fc782653cad3fdbc3
altrios <famartinez.ti@gmail.com>
2021-08-26 22:11:29 -0500

    fix assitance view

--- src/Components/Classes/Assistance.jsx
+++ src/Components/Classes/Assistance.jsx
@@ -44,7 +44,7 @@ let userAsistance = [{
 const Assistance = (props) => {
     const [selectedDate, setSelectedDate] = React.useState(new Date('2021-08-18'));
     const [students, setSutents] = useState([])
-    
+
     const [stdent, setStdent] = useState([])
     const [assistance, setAssistance] = useState([]);
     const [state, setState] = useContext(AppContext)
@@ -54,163 +54,157 @@ const Assistance = (props) => {
         setSelectedDate(date);
         //setAssistance(true)
         studentsAsistance();
-        
+
     };
 
-    const studentsAsistance =()=>{
-    
+    const studentsAsistance = () => {
+
 
         var axios = require('axios');
         var data = JSON.stringify({
-          "data": {
-            "type": "activity_assistences",
-            "attributes": {
-              "activity_id": "1",
-              "student_id": "1",
-              "teacher_id": "1",
-              "is_present": 1,
-              "createdAt": "2021-04-02T00:00:00.000000Z",
-              "updatedAt": "2021-04-02T00:00:00.000000Z"
+            "data": {
+                "type": "activity_assistences",
+                "attributes": {
+                    "activity_id": "1",
+                    "student_id": "1",
+                    "teacher_id": "1",
+                    "is_present": 1,
+                    "createdAt": "2021-04-02T00:00:00.000000Z",
+                    "updatedAt": "2021-04-02T00:00:00.000000Z"
+                }
             }
-          }
         });
-        
+
         var config = {
-          method: 'get',
-          url: 'http://cerebelloback.echilateral.com/api/v1/activity_assistences',
-          headers: { 
-            'Accept': 'application/vnd.api+json', 
-            'Content-Type': 'application/vnd.api+json', 
-            'Authorization': 'Bearer ' + state.token
-          },
-          data : data
+            method: 'get',
+            url: 'http://cerebelloback.echilateral.com/api/v1/activity_assistences',
+            headers: {
+                'Accept': 'application/vnd.api+json',
+                'Content-Type': 'application/vnd.api+json',
+                'Authorization': 'Bearer ' + state.token
+            },
+            data: data
         };
-        
+
         axios(config)
-        .then(function (response) {
-            setAssistance(response.data.data)
-          //console.log(response.data.data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-        
-    
-}
-let name ='';
+            .then(function (response) {
+                setAssistance(response.data.data)
+                document.getElementById("attended").innerHTML='<h3><bold>Asistencia de la fecha '+selectedDate+'</bold></h3>'
+                //console.log(response.data.data);
+                assistance.map((data, index) => {
+console.log(selectedDate)
+                    if ((new Date(selectedDate).getDate() == new Date(data.attributes.createdAt).getDate())
+                        && (new Date(selectedDate).getMonth() + 1 == new Date(data.attributes.createdAt).getMonth() + 1)
+                        && (new Date(selectedDate).getFullYear() == new Date(data.attributes.createdAt).getFullYear())
+                    ) {
+
+console.log(data.attributes.student_id)
+                        var axios = require('axios');
+
+                        var config = {
+                            method: 'get',
+                            url: 'http://cerebelloback.echilateral.com/api/v1/students/' + data.attributes.student_id,
+                            headers: {
+                                'Accept': 'application/vnd.api+json',
+                                'Content-Type': 'application/vnd.api+json',
+                                'Authorization': 'Bearer ' + state.token
+                            }
+                        };
+
+                        axios(config)
+                            .then(function (response) {
+
+                                console.log(response.data.data.attributes.user_name)
+
+                                setSutents(response.data.data.attributes);
+                                document.getElementById("attended").innerHTML+='<br><span>'+response.data.data.attributes.user_name+'</span>'
+                                //console.log(students)
+
+                            })
+                            .catch(function (error) {
+                                setSutents('');
+                                console.log(state.token)
+                                console.log(error);
+                            });
+
+
+                    }
+
+                })
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
+    }
+    let name = '';
 
     if (sessionStorage.getItem("token") === null) {
 
         return <Redirect to='/login' />;
-    }else{
-    return (
-        <div className="App">
-            <div className={classes.content}>
-                <List componet='nav'>
-                    <ListItem >
-                        <Grid xs={12}>
-                            <div>
-                                <Box className={classes.title} >
-                                    <div className={classes.title} >
-                                        <h3>Cronograma de Actividades</h3>
-                                        <h3>Crear Actividad</h3>
-                                    </div>
-                                </Box>
-                            </div>
-                        </Grid>
-                    </ListItem>
-                    <ListItem >
-                        <Grid xs={12} >
-                            <Box
-                            >
-                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                    <DatePicker
-                                        inputVariant="outlined"
-                                        variant="inline"
-                                        format="MM/dd/yyyy"
-                                        margin="normal"
-                                        id="date-picker-inline"
-                                        label=""
-                                        value={selectedDate}
-                                        onChange={handleDateChange}
-                                        KeyboardButtonProps={{
-                                            'aria-label': 'change date',
-                                        }}
-                                    />
-                                </MuiPickersUtilsProvider>
-                                <Button></Button>
-                            </Box>
-                        </Grid>
-
-                    <div container className={classes.content} >
-                                    <Grid>
-                                        <Box>
-                                            <Link  >
-                                                <Button  >
-                                                    <ListItemIcon>
-                                                        <CloudQueue />
-                                                    </ListItemIcon>
-                                                    <b>Mi Clase</b></Button></Link>
-                                        </Box>
-                                    </Grid>
+    } else {
+        return (
+            <div className="App">
+                <div className={classes.content}>
+                    <List componet='nav'>
+                        <ListItem >
+                            <Grid xs={12}>
+                                <div>
+                                    <Box className={classes.title} >
+                                        <div className={classes.title} >
+                                            <h3>Cronograma de Actividades</h3>
+                                            <h3>Crear Actividad</h3>
+                                        </div>
+                                    </Box>
                                 </div>
-                    </ListItem>
-                </List>
-                {
-                    
-                            assistance.map((data, index) => {
-                               // console.log(new Date(selectedDate).getDate()+'/'+new Date(selectedDate).getMonth()+1+'/'+new Date(selectedDate).getYear())
-                               /* console.log(new Date(selectedDate).getDate())
-                                console.log(new Date(selectedDate).getMonth()+1)
-                                console.log(new Date(selectedDate).getFullYear() )
-                               // console.log(new Date(selectedDate))
-                                console.log(new Date(data.attributes.createdAt).getDate())
-                                console.log(new Date(data.attributes.createdAt).getMonth()+1)
-                                console.log(new Date(data.attributes.createdAt).getFullYear())*/
-                               // console.log(data)
-                                //console.log(props.location.Activity.cohorteID)
-                                //console.log(props.location)
-                                  //console.log(new Date(data.attributes.createdAt).getDay()+1+'/'+new Date(data.attributes.createdAt).getMonth()+1+'/'+new Date(data.attributes.createdAt).getYear()+1)
-                               if((new Date(selectedDate).getDate()==new Date(data.attributes.createdAt).getDate())
-                               &&(new Date(selectedDate).getMonth()+1==new Date(data.attributes.createdAt).getMonth()+1)
-                               &&(new Date(selectedDate).getFullYear()==new Date(data.attributes.createdAt).getFullYear())
-                               ){
-                                var axios = require('axios');
+                            </Grid>
+                        </ListItem>
+                        <ListItem >
+                            <Grid xs={12} >
+                                <Box
+                                >
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <DatePicker
+                                            inputVariant="outlined"
+                                            variant="inline"
+                                            format="MM/dd/yyyy"
+                                            margin="normal"
+                                            id="date-picker-inline"
+                                            label=""
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </MuiPickersUtilsProvider>
+                                    <Button></Button>
+                                </Box>
+                            </Grid>
 
-                                var config = {
-                                    method: 'get',
-                                    url: 'http://cerebelloback.echilateral.com/api/v1/students/'+data.attributes.student_id,
-                                    headers: {
-                                        'Accept': 'application/vnd.api+json',
-                                        'Content-Type': 'application/vnd.api+json',
-                                        'Authorization': 'Bearer ' + state.token
-                                    }
-                                };
+                            <div container className={classes.content} >
+                                <Grid>
+                                    <Box>
+                                        <Link  >
+                                            <Button  >
+                                                <ListItemIcon>
+                                                    <CloudQueue />
+                                                </ListItemIcon>
+                                                <b>Mi Clase</b></Button></Link>
+                                    </Box>
+                                </Grid>
+                            </div>
+                        </ListItem>
+                    </List>
+                    
+                            <div><span id="attended">aja</span></div>
                         
-                                axios(config)
-                                    .then(function (response) {
-                                        
-                                        console.log(response.data.data.attributes.user_name)
-                        
-                                        setSutents(response.data.data.attributes.user_name);
-                        
-                                    })
-                                    .catch(function (error) {
-                                        console.log(state.token)
-                                        console.log(error);
-                                    });
-                        
-                                  
-                               } 
-                               return(
-                                <div><span>{students}</span></div>
-                            )
-                            })
-                        }
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 }
 
 export default Assistance;
